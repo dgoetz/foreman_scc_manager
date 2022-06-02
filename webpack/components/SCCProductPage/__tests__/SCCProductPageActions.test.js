a1d0d6eafdb3822c2176dcf16c08734a80f974ca
Nadja Heitmann <nadjah@atix.de>
2022-06-02 11:55:36 +0000

    Remove unneeded test folder

--- webpack/components/SCCProductPage/__tests__/SCCProductPageActions.test.js
+++
@@ -1,9 +0,0 @@
-import { testActionSnapshotWithFixtures } from 'react-redux-test-utils';
-import { changeBool } from '../SCCProductPageActions';
-
-const fixtures = {
-  'should changeBool': () => changeBool({ bool: true }),
-};
-
-describe('SCCProductPage actions', () =>
-  testActionSnapshotWithFixtures(fixtures));
